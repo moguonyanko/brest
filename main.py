317f326fa8515d563927c3e2744c028041b940cf
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-12-28 01:15:55 +0900

    Modified FastAPI first sample

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-app = FastAPI()
+my_sample_app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = None
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
+@my_sample_app.get("/")
+async def root():
+    return {"message": "𩸽を𠮟る𠮷野家〜髙﨑"}

22c22911643e971f78077999c7c9225f7486c9c4
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-12-31 00:03:44 +0900

    Renamed app name

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from fastapi import FastAPI
 
-brest_app = FastAPI()
+brest_service = FastAPI()
 APP_ROOT = "/brest/"
 
-@brest_app.get("/")
+@brest_service.get("/")
 async def root():
     return "Hello FastAPI"
 
-@brest_app.get(APP_ROOT)
+@brest_service.get(APP_ROOT)
 async def brest_root():
     return {"message": "Brest Root"}
 
-@brest_app.get(APP_ROOT + "echo/{message}")
+#関数名が衝突する場合は定義された順に評価される。
+@brest_service.get(APP_ROOT + "echo/hello")
+async def brest_echo():
+    return {"message": 'World'}
+
+@brest_service.get(APP_ROOT + "echo/{message}")
 async def brest_echo(message: str):
     return {"message": message}
 
 #関数名が他と重複してもエラーにはならない。しかしドキュメントがややこしくなるので好ましくない。
-@brest_app.get(APP_ROOT + "pow/{number}")
+@brest_service.get(APP_ROOT + "pow/{number}")
 async def brest_pow(number: int): #型情報を書くことで整数でない値を渡した時に詳細なエラーを表示できる。
     return {"result": number ** 2}
+

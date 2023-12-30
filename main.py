ec3a841da5be4de39a7daa3faa7db1d9b7304da1
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-12-30 13:37:44 +0900

    Added sample pow function

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 
 brest_app = FastAPI()
+APP_ROOT = "/brest/"
 
 @brest_app.get("/")
 async def root():
-    return "Hello 𩸽を𠮟る𠮷野家〜髙﨑〜彁"
+    return "Hello FastAPI"
 
-@brest_app.get("/brest/")
+@brest_app.get(APP_ROOT)
 async def brest_root():
     return {"message": "Brest Root"}
+
+@brest_app.get(APP_ROOT + "echo/{message}")
+async def brest_echo(message: str):
+    return {"message": message}
+
+#関数名が他と重複してもエラーにはならない。しかしドキュメントがややこしくなるので好ましくない。
+@brest_app.get(APP_ROOT + "pow/{number}")
+async def brest_pow(number: int): #型情報を書くことで整数でない値を渡した時に詳細なエラーを表示できる。
+    return {"result": number ** 2}

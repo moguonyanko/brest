4b503a2b403aaff4c0c2453e4c207c1446542b7e
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2024-01-01 13:53:43 +0900

    Added FastAPI query parameter example

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ https://fastapi.tiangolo.com/ja/tutorial/
 from fastapi import FastAPI
 from enum import Enum
 import json
+from typing import Union
 
 brest_service = FastAPI()
 APP_ROOT = "/brest/"
@@ -17,7 +18,7 @@ async def root():
 async def brest_root():
     return {"message": "Brest Root"}
 
-#関数名が衝突する場合は定義された順に評価される。
+# 関数名が衝突する場合は定義された順に評価される。
 @brest_service.get(APP_ROOT + "echo/hello")
 async def brest_echo():
     return {"message": 'World'}
@@ -26,9 +27,9 @@ async def brest_echo():
 async def brest_echo(message: str):
     return {"message": message}
 
-#関数名が他と重複してもエラーにはならない。しかしドキュメントがややこしくなるので好ましくない。
+# 関数名が他と重複してもエラーにはならない。しかしドキュメントがややこしくなるので好ましくない。
 @brest_service.get(APP_ROOT + "pow/{number}")
-async def brest_pow(number: int): #型情報を書くことで整数でない値を渡した時に詳細なエラーを表示できる。
+async def brest_pow(number: int):  # 型情報を書くことで整数でない値を渡した時に詳細なエラーを表示できる。
     return {"result": number ** 2}
 
 # class ModelName():
@@ -40,7 +41,7 @@ async def brest_pow(number: int): #型情報を書くことで整数でない値
 #     barnet = ModelName("bar")
 #     baznet = ModelName("baz")
 
-#strを継承させないとパスパラメータの値から定義済みのクラス（ここではModels）を得ることができない。
+# strを継承させないとパスパラメータの値から定義済みのクラス（ここではModels）を得ることができない。
 class Models(str, Enum):
     foonet = "foo"
     barnet = "bar"
@@ -56,5 +57,62 @@ async def get_model_description(model: Models):
 
 @brest_service.get(APP_ROOT + "files/{file_path:path}")
 async def read_json(file_path: str):
-    with open(f"/{file_path}" , encoding="UTF-8") as f:
+    with open(f"/{file_path}", encoding="utf-8") as f:
         return json.loads(f.read())
+
+sample_items = [
+    {
+        "item_name": "𩸽",
+        "price": 300
+    },
+    {
+        "item_name": "𠮷野家",
+        "price": 10000
+    },
+    {
+        "item_name": "髙﨑〜彁",
+        "price": 200
+    }
+]
+
+'''
+Unionでオプションパラメータを定義できる。Union[str, None]の場合はstrのパラメータが指定されればそれを使い
+パラメータが指定されなければNoneになる。
+'''
+@brest_service.get(APP_ROOT + "items/")
+async def read_item(start: int = 0, end: int = len(sample_items),
+                    description: Union[str, None] = None,
+                    nameonly: bool = False):
+    items = sample_items[start: end]
+    if nameonly:
+        items = [item["item_name"] for item in items]
+    if description:
+        items.append({"description": description})
+    return items
+
+sample_users = {
+    "soumu": {
+        "mike": {
+            "name": "Mike",
+            "score": 90
+        },
+        "taro": {
+            "name": "Taro",
+            "score": 95
+        }
+    },
+    "jinji": {
+        "joe": {
+            "name": "Joe",
+            "score": 88
+        }
+    }
+}
+
+# デフォルト値を指定していないパラメータは必須パラメータとなる。
+@brest_service.get(APP_ROOT + "groups/{group_name}/users/{user_name}")
+async def get_user(group_name: str, user_name: str, score: Union[int, None] = None):
+    user = sample_users.get(group_name).get(user_name)
+    if user == None or score and user.get("score") < score:
+        return {}
+    return user

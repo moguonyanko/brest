0cb4e1262f93969cb2dd3a81553cd35efe464a81
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-03-03 13:15:55 +0900

    Add get cordinates function

--- mygis.py
+++ mygis.py
@@ -1,22 +1,25 @@
-'''
-参考:
-https://fastapi.tiangolo.com/tutorial/bigger-applications/
-'''
 from fastapi import FastAPI
 
 app = FastAPI(
     title="My GIS API",
-    description="GISに関連する計算処理をRESTによるAPIで提供する。",
+    description="GISの計算機能をREST APIで提供する。",
     summary="GIS REST API",
     version="0.0.1"
 )
 
-@app.get("/hello/", tags=["test"])
-async def request_ping():
-    return { "message": "Hello My GIS API!" }
+def get_coordinates(feature, index: int = 0) -> list:
+    if "geometry" in feature:
+        return feature["geometry"]["coordinates"]
+    else:
+        return feature["features"][index]["geometry"]["coordinates"]
 
 #GeoJSONの形式でリクエストしたいのでPOSTにしている。
 @app.post("/pointsideofline/", tags=["geometry"])
 async def get_point_side_of_line(line: dict, point: dict):
-    return { "message": "Implement now" }
+    line_coordinates = get_coordinates(line)
+    point_coordinates = get_coordinates(point)
+    return {"line_coordinates": line_coordinates, "point_coordinates": point_coordinates}
 
+@app.get("/hellomygis/", tags=["test"])
+async def request_ping():
+    return { "message": "Hello My GIS!" }

14881fb16c68fe39918b7b6ba1e96315084ac07e
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-03-05 00:39:38 +0900

    Add point side function

--- gis.py
+++ gis.py
@@ -15,12 +15,22 @@ def get_coordinates(feature, index: int = 0) -> list:
     else:
         return feature["features"][index]["geometry"]["coordinates"]
 
+def point_on_which_side(px, py, lx1, ly1, lx2, ly2) -> int:
+    cp = (px - lx1) * (ly2 - ly1) - (py - ly1) * (lx2 - lx1)
+    if cp > 0: #right
+        return 1 
+    elif cp < 0: #left
+        return -1
+    else: #on the line
+        return 0
+
 #GeoJSONの形式でリクエストしたいのでPOSTにしている。
 @app.post("/pointsideofline/", tags=["geometry"])
 async def get_point_side_of_line(line: dict, point: dict):
-    line_coordinates = get_coordinates(line)
-    point_coordinates = get_coordinates(point)
-    return {"line_coordinates": line_coordinates, "point_coordinates": point_coordinates}
+    lps = get_coordinates(line)
+    pps = get_coordinates(point)
+    side = point_on_which_side(pps[1], pps[0], lps[0][1], lps[0][0], lps[1][1], lps[1][0])
+    return {"side": side}
 
 @app.get("/hellogis/", tags=["test"])
 async def request_ping():

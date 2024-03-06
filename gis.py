5bae0be3a1cc360eaacf9e911eed0cd85efc4179
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-03-06 23:44:55 +0900

    Modify coords variables

--- gis.py
+++ gis.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from typing import Union, Tuple
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI(
     title="Brest GIS API",
@@ -15,13 +16,44 @@ def get_coordinates(feature, index: int = 0) -> list:
     else:
         return feature["features"][index]["geometry"]["coordinates"]
 
+def get_gradient(lx1: Union[int, float], ly1: Union[int, float], 
+                 lx2: Union[int, float], ly2: Union[int, float]) -> Union[int, float]:
+    dy = ly2 - ly1
+    dx = lx2 - lx1
+    gradient = dy / dx
+    return gradient
+
+def get_intercept(lx1: Union[int, float], ly1: Union[int, float], 
+                  gradient: Union[int, float]) -> Union[int, float]:
+    return ly1 - gradient * lx1
+
+'''
+点の座標が方程式を満たしていたら点が直線上にあると判定する。
+'''
+def is_on_line(px, py, lx1, ly1, lx2, ly2) -> bool:
+    gradient = get_gradient(lx1, ly1, lx2, ly2)
+    intercept = get_intercept(lx1, ly1, gradient)
+
+    # 点Pが方程式を満たしているかどうか
+    if gradient == 0:
+        is_on_line = px == lx1
+    elif intercept == 0:
+        is_on_line = py == gradient * px
+    else:
+        is_on_line = py == gradient * px + intercept
+    
+    return is_on_line
+
+'''
+ベクトルの外積から引数の点が線分を挟んでどちら側にあるのかを求める。
+'''
 def point_on_which_side(px, py, lx1, ly1, lx2, ly2) -> int:
     cp = (px - lx1) * (ly2 - ly1) - (py - ly1) * (lx2 - lx1)
     if cp > 0: #right
         return 1 
     elif cp < 0: #left
         return -1
-    else: #on the line
+    else: #on the line #既にis_on_line()で判定しているのでここには到達しない。
         return 0
 
 #GeoJSONの形式でリクエストしたいのでPOSTにしている。
@@ -29,8 +61,20 @@ def point_on_which_side(px, py, lx1, ly1, lx2, ly2) -> int:
 async def get_point_side_of_line(line: dict, point: dict):
     lps = get_coordinates(line)
     pps = get_coordinates(point)
-    side = point_on_which_side(pps[1], pps[0], lps[0][1], lps[0][0], lps[1][1], lps[1][0])
-    return {"side": side}
+    if len(lps) == 0 or len(pps) == 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                            detail="点と線分の座標は必須です。")
+    px = pps[1]
+    py = pps[0]
+    lx1 = lps[0][1]
+    ly1 = lps[0][0] 
+    lx2 = lps[1][1] 
+    ly2 = lps[1][0]
+    if is_on_line(px, py, lx1, ly1, lx2, ly2):
+        return {"side": 0}
+    else:
+        side = point_on_which_side(px, py, lx1, ly1, lx2, ly2)
+        return {"side": side}
 
 @app.get("/hellogis/", tags=["test"])
 async def request_ping():
--- test_gis.py
+++ test_gis.py
@@ -7,3 +7,9 @@ def test_get_hellogis():
     response = test_client.get("/hellogis/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello Brest GIS!"}
+
+def test_get_point_side_of_line():
+    response = test_client.post("/pointsideofline/",
+                                json={"point":{"type":"Feature","properties":{},"geometry":{"coordinates":[139.751363304702,35.65771000179585],"type":"Point"}},"line":{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"coordinates":[[139.7551995346509,35.66006748781244],[139.75693265376594,35.65465624195437]],"type":"LineString"}}]}})
+    assert response.status_code == 200
+    assert response.json() == {"side": -1}

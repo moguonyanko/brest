01a16226fb45204bb1e4b98c4114ed1e4d01a13e
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2025-04-05 08:49:18 +0900

    ローカルの音声ファイルから文字起こしするテスト追加

--- test_genaiapi.py
+++ test_genaiapi.py
@@ -1,3 +1,4 @@
+import os
 from fastapi.testclient import TestClient
 from genaiapi import app, get_genai_client, get_generate_image_model_name
 from google import genai
@@ -8,6 +9,9 @@ import base64
 
 test_client = TestClient(app)
 
+def get_user_dir():
+  return os.path.expanduser("~")
+
 def test_generate_text():
   response = test_client.post("/generate/text/",
                               json={"contents":"What is one plus one?"})
@@ -15,3 +19,25 @@ def test_generate_text():
   text = response.json()["results"]
   print(text)
   assert "two" in text.lower()
+
+'''
+ローカルの音声ファイルから文字起こしできるかどうかのテスト
+参考:
+https://ai.google.dev/gemini-api/docs/audio?hl=ja&lang=python#inline-data
+'''
+def test_generate_text_from_audio():
+  with open(f"{get_user_dir()}/share/audio/sampleaudio.m4a", 'rb') as f:
+      audio_bytes = f.read()
+
+  response = get_genai_client().models.generate_content(
+    model='gemini-2.0-flash',
+    contents=[
+      'Describe this audio clip',
+      types.Part.from_bytes(
+        data=audio_bytes,
+        mime_type='audio/mp4',
+      )
+    ]
+  )
+
+  assert response.text is not None

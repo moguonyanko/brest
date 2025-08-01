c2dc18e6cdd354606362cdcc5631ef3b3f3f8205
moguonyako <moguonyanko@gmail.com>
2025-08-01 10:15:38 +0900

    タイトルを得るAPIの作成中
    エラーが発生するためまだ正常に動作しない。

--- test_webscraping.py
+++ test_webscraping.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from webscraping import app
+import pytest
 
 test_client = TestClient(app)
 
@@ -15,3 +16,17 @@ def test_get_page_contents():
     assert response.status_code == 200
     contents = response.json()["contents"]
     assert contents is not None
+
+
+def test_error_http_exception_when_not_found_title():
+    """存在しないページのタイトルを得ようとした時に500エラーになることのテストです。"""
+    response = test_client.get("/pagetitle/?url=http://localhost/errorsite")
+    assert response.status_code == 400
+
+
+def test_get_page_title():
+    response = test_client.get("/pagetitle/?url=http://localhost/webxam")
+    assert response.status_code == 200
+    title = response.json()["title"]
+    assert title is not None
+    assert title == "WebXam"
--- webscraping.py
+++ webscraping.py
@@ -1,3 +1,10 @@
+"""WebスクレイピングのAPIを提供します。
+
+参考資料:
+PythonによるWebスクレイピング 第3版
+https://www.oreilly.co.jp/books/9784814401222/
+"""
+
 import json
 import requests
 import time
@@ -5,11 +12,8 @@ from io import BytesIO
 from typing import Annotated
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from google import genai
-from google.genai import types
-from google.genai.types import Tool, GenerateContentConfig, GoogleSearch, UrlContext
 from PIL import Image
-from fastapi import FastAPI, HTTPException, Body, Response
+from fastapi import FastAPI, HTTPException, Body, Response, status, Depends
 from fastapi.responses import StreamingResponse, FileResponse
 from starlette.background import BackgroundTask
 from fastapi import File, UploadFile, WebSocket, WebSocketDisconnect
@@ -20,6 +24,8 @@ from pathlib import Path
 import uuid
 import os
 from urllib.request import urlopen
+from urllib.error import URLError
+from bs4 import BeautifulSoup
 
 app = FastAPI(
     title="Brest Web Scraping API",
@@ -36,7 +42,30 @@ async def get_hello_webscraping():
     return {"message": "Hello Brest Web Scraping!"}
 
 
-@app.get("/pagecontents/", tags=["url"])
-async def get_page_contents(url: str):
-    contents = urlopen(url)
-    return {"contents": contents.read()}
+def read_all_contents(url: str) -> str:
+    try:
+        contents = urlopen(url)
+    except URLError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e.reason)
+    return contents.read()
+
+
+@app.get("/pagecontents/", tags=["url"], response_model=dict[str, str])
+async def ws_get_page_contents(url: str):
+    return {"contents": read_all_contents(url)}
+
+
+def get_title(contents) -> str:
+    try:
+        bs = BeautifulSoup(contents, "html.parser")
+        return bs.body.h1
+    except AttributeError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=e.args[0]
+        )
+
+
+@app.get("/pagetitle/", tags=["url"], response_model=dict[str, str])
+async def ws_get_page_title(url: str):
+    contents = read_all_contents(url)
+    return {"title": get_title(contents)}
